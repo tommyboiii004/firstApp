24fdd6e60a5c76a0bdae61c4c1eaefd65581eb73
tommyboiiii004 <thomastharun004@gmail.com>
2023-01-13 22:24:25 +0530

    Layout decisions in App.jsx

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import styles from './style'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Here a navigation bar will appear
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero part of the page comes here
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      Stats
+      Business
+      Billing
+      CardDeal
+      Testimonial
+      Clients
+      CTA
+      Footer
+
+    </div>
+
+
+  </div>
+);
+
 
 export default App
