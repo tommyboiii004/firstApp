118ce39fd5db25a8c1f992d37340d9714d3064bc
tommyboiiii004 <thomastharun004@gmail.com>
2023-04-21 22:22:11 +0530

    Cleaning and structuring folders

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
-import { close, logo, menu } from '../assets'
-import { navLinks } from '../constants'
-import styles from '../style'
+import { useState } from 'react';
+import { close, menu } from '../assets';
+import { navLinks } from '../constants';
+import styles from '../style';
 
 const Navbar = () => {
+    const [active, setActive] = useState("Home");
     const [toggle, setToggle] = useState(false);
 
     return (
@@ -29,28 +30,34 @@ const Navbar = () => {
 
 
 
-            <div className='sm:hidden flex flex-1 justify-end items-center'>
-                <img src={toggle ? close : menu}
-                    alt='close or menu'
-                    className='w-[28px] h-[28px] object-contain'
-                    onClick={() => setToggle((prev) => !prev)}
+            <div className="sm:hidden flex flex-1 justify-end items-center">
+                <img
+                    src={toggle ? close : menu}
+                    alt="menu"
+                    className="w-[28px] h-[28px] object-contain"
+                    onClick={() => setToggle(!toggle)}
                 />
 
-                <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
-                    <ul className="list-none flex-col justify-end items-center flex-1">
+                <div
+                    className={`${!toggle ? "hidden" : "flex"
+                        } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+                >
+                    <ul className="list-none flex justify-end items-start flex-1 flex-col">
                         {navLinks.map((nav, index) => (
-                            <li key={nav.id}
-                                className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'} text-white`}>
-                                <a href={`#${nav.id}`}>
-                                    {nav.title}
-                                </a>
+                            <li
+                                key={nav.id}
+                                className={`font-poppins font-medium cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"
+                                    } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                                onClick={() => setActive(nav.title)}
+                            >
+                                <a href={`#${nav.id}`}>{nav.title}</a>
                             </li>
                         ))}
                     </ul>
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
